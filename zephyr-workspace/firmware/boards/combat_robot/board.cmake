05cc70acabcf0f1a7d1fa4f386b16ad1dd803d07
Dan Collins <dan@collinsnz.com>
2024-11-18 17:38:37 +1300

    Adds combat board device tree and updates main to load devices

---
+++ zephyr-workspace/firmware/boards/combat_robot/board.cmake
@@ -0,0 +1,8 @@
+# keep first
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(jlink "--device=STM32L412CB" "--speed=4000")
+
+# keep first
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
--- zephyr-workspace/firmware/src/main.c
+++ zephyr-workspace/firmware/src/main.c
@@ -1,7 +1,20 @@
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+
+/* Placeholders to make sure our device tree is working. */
+
+static const struct device *rf_uart_dev = DEVICE_DT_GET(DT_CHOSEN(combat_rf_uart));
+static const struct device *esc_uart_dev = DEVICE_DT_GET(DT_CHOSEN(combat_esc_uart));
+static const struct device *i2c_dev = DEVICE_DT_GET(DT_CHOSEN(combat_i2c));
+static const struct device *spi_flash = DEVICE_DT_GET(DT_CHOSEN(combat_spi_flash));
 
 int main(void)
 {
+    __ASSERT(device_is_ready(rf_uart_dev), "RF UART device not ready\n");
+    __ASSERT(device_is_ready(esc_uart_dev), "ESC UART device not ready\n");
+    __ASSERT(device_is_ready(i2c_dev), "I2C device not ready\n");
+    __ASSERT(device_is_ready(spi_flash), "SPI flash device not ready\n");
+
     printk("Hello, world!\n");
     return 0;
 }
